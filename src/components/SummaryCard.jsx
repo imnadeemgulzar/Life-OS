1a1d2350923fda318f17eea9928eaa86d2147338
imnadeemgulzar <nadeembht777@gmail.com>
2025-10-23 14:14:22 +0530

    added the modal for new items and new notes

--- src/components/SummaryCard.jsx
+++ src/components/SummaryCard.jsx
@@ -1,15 +1,58 @@
-const SummaryCard = ({ title, items, type }) => {
+import chevronIcon from "../assets/chevron-down-solid.svg";
+
+const SummaryCard = ({
+  title,
+  items,
+  type,
+  handleStatus,
+  toggleSnippet,
+  openNoteId,
+  openModal,
+}) => {
   return (
     <div className="bg-white px-6 py-4 rounded-lg shadow-sm w-72">
+     <div className="flex justify-between items-center">
       <h2 className="text-xl font-bold text-gray-700 mb-3">{title}</h2>
+      <h3 className="text-2xl font-medium text-blue-700 mb-3 cursor-pointer" onClick={() => openModal(title)}>+</h3>
+      </div> 
       <ul className="flex flex-col gap-2">
-        {items.map(item => (
-          <li
-            key={item.id}
-            className="px-3 py-2 bg-gray-100 rounded flex justify-between items-center"
-          >
-            {item.title}
-            {type === "task" && <input type="checkbox" />}
+        {items.map((item) => (
+          <li key={item.id} className="px-3 py-2 bg-gray-100 rounded">
+            <div className="flex justify-between items-center">
+              <h3
+                className={item.completed ? "line-through text-gray-400" : ""}
+              >
+                {item.title}
+              </h3>
+
+              {type === "task" && (
+                <input
+                  type="checkbox"
+                  className="cursor-pointer"
+                  onChange={() => handleStatus(item.id)}
+                  checked={item.completed}
+                />
+              )}
+
+              {type === "habit" && (
+                <progress value={item.progress} className="h-2" />
+              )}
+
+              {type === "note" && (
+                <img
+                  src={chevronIcon}
+                  alt="expandIcon"
+                  className={`w-4 h-4 cursor-pointer transition-transform duration-300 ${
+                    openNoteId === item.id ? "rotate-180" : "rotate-0"
+                  }`}
+                  onClick={() => toggleSnippet(item.id)}
+                />
+              )}
+            </div>
+
+            {openNoteId === item.id && (
+              <div className="text-sm text-gray-500 mt-1">{item.snippet}</div>
+            )}
           </li>
         ))}
       </ul>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,101 @@
-import SummaryCard from '../components/SummaryCard'
-    const tasks = [{id:1,title:'Task-1'},{id:2,title:'Task-2'},{id:3,title:'Task-3'}]
-    const notes = [{id:1,title:'note-1'},{id:2,title:'note-2'},{id:3,title:'note-3'}]
-    const habits = [{id:1,title:'habit-1'},{id:2,title:'habit-2'},{id:3,title:'habit-3'}]
+import { useState } from "react";
+import SummaryCard from "../components/SummaryCard";
+const habits = [
+  { id: 1, title: "habit-1", progress: 0.1 },
+  { id: 2, title: "habit-2", progress: 0.2 },
+  { id: 3, title: "habit-3", progress: 0 },
+];
 const Dashboard = () => {
+  const [tasks,setTasks] = useState([
+  { id: 1, title: "Task-1", completed: false },
+  { id: 2, title: "Task-2", completed: false },
+  { id: 3, title: "Task-3", completed: false },
+]);
+const [notes,setNotes] = useState([
+  { id: 1, title: "note-1", snippet: "note-1 text  cum repudiandae labore, maxime dolorem animi laboriosam ullam ipsa nostrum voluptatibus odit" },
+  { id: 2, title: "note-2", snippet: "note-2 text" },
+  { id: 3, title: "note-3", snippet: "note-3 text" },
+])
+const [activeCard,setActiveCard] = useState("");
+const [isModalOpen,setIsModalOpen] = useState(false);
+const [openNoteId,setOpenNoteId] = useState(null);
+const [newTask,setNewTask] = useState('')
+const [taskStatus,setTaskStatus] = useState('')
+  const handleStatus = (id) => {
+    setTasks(prev => prev.map(item => item.id === id ? {...item, completed:!item.completed} : item))
+  };
+  const toggleSnippet = (id) => {
+    setOpenNoteId(prev => 
+      prev === id ? null : id
+    );
+  }
+  const openModal = (title) => {
+    setActiveCard(title);
+    setIsModalOpen(true);
+  }
+  
+  const closeModal = (title) => {
+    // setActiveCard(title);
+    setIsModalOpen(false);
+  }
+  const handleNewTask = () => {
+    activeCard === "Tasks" &&  setTasks([...tasks,{id:18,title:newTask,completed:taskStatus}])
+    activeCard === "Notes" &&  setNotes([...notes,{id:9,title:'uii',snippet:'lord'}])
+    
+  }
+
   return (
     <div className="flex flex-wrap gap-5 p-5">
-      <SummaryCard title="Tasks" items={tasks} type="task" />
-      <SummaryCard title="Notes" items={notes} type="note" />
-      <SummaryCard title="Habits" items={habits} type="habit" />
+      <SummaryCard
+        title="Tasks"
+        items={tasks}
+        type="task"
+        handleStatus={handleStatus}
+        openModal={openModal}
+        CloseModal={closeModal}
+      />
+      <SummaryCard title="Notes" items={notes} type="note" toggleSnippet={toggleSnippet} openNoteId={openNoteId} openModal={openModal}/>
+      <SummaryCard title="Habits" items={habits} type="habit" openModal={openModal}/>
+      {isModalOpen && (
+  <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50">
+    <div className="bg-white rounded-lg p-6 w-80 shadow-lg">
+      <h2 className="text-lg font-semibold mb-4">Add Task to {activeCard}</h2>
+      {console.log(activeCard)}
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Enter task name"
+        className="border w-full px-3 py-2 rounded mb-4 outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      {activeCard === "Tasks" && <select className="my-4" name="status" id="status" value={taskStatus} onChange={(e) => setTaskStatus(e.target.value)}>
+        <option value="pending">Pending</option>
+        <option value="in Progress">In Progress</option>
+        <option value="done">Done</option>
+      </select> }
+      {activeCard === "Notes" && <> <input type="text" placeholder="Note Title" 
+        className="border w-full px-3 py-2 rounded mb-4 outline-none focus:ring-2 focus:ring-blue-500"
+      /> <input type="text" placeholder="Your Note"
+        className="border w-full px-3 py-2 rounded mb-4 outline-none focus:ring-2 focus:ring-blue-500"
+      
+      />
+         </>}
+      <div className="flex justify-end gap-2">
+        <button onClick={closeModal} className="px-4 py-2 rounded bg-gray-200">
+          Cancel
+        </button>
+        <button
+          onClick={() => handleNewTask()}
+          className="px-4 py-2 rounded bg-blue-600 text-white"
+        >
+          Add
+        </button>
+      </div>
+    </div>
+  </div>
+)}
     </div>
   );
-}
+};
 
-export default Dashboard
+export default Dashboard;
